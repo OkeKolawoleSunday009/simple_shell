0f485107931c4e2791c4855a99a8127889893259
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2021-10-21 13:32:03 -0700

    coming up
    Co-authored-by: Daniel Omosefunmi <omosefunmid@gmail.com>

---
+++ printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/** 
+ * main - function that prints te result
+ * Return: Always return 0
+ */
+int main(void)
+{
+	printf("This is a simple betty style coding");
+	return (0);
+}
